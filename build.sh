46838194a554591adaaa28f120fb1d4673067963
Maslow Vladislav <70482199+masloffvs@users.noreply.github.com>
2024-05-25 02:00:10 +0300

    add build.sh

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ "$(uname)" != "Linux" -a "$(uname)" != "Darwin" ]; then
+    echo "Only Linux and macOS are supported" >&2
+    exit 0
+fi
+
+cd web || exit
+
+npm install
+npm run build
+
