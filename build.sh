e840fc1c39fb443d669fb424bbe43dbb0fb0ab59
Maslow Vladislav <70482199+masloffvs@users.noreply.github.com>
2024-06-03 19:35:32 +0300

    add ness. paths and depens

--- build.sh
+++ build.sh
@@ -4,10 +4,14 @@
 mkdir -p plugins
 mkdir -p public
 
+touch plugins.toml
+
 if [ "$(uname)" != "Linux" -a "$(uname)" != "Darwin" ]; then
     echo "Only Linux and macOS are supported" >&2
     exit 0
 fi
+
+docker compose build
 
 cd web || exit
 
